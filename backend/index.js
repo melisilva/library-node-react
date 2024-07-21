a5a0e19209ff5705dba7fe8281bffffe53ee4fd2
melisilva <55757043+melisilva@users.noreply.github.com>
2024-07-21 11:47:25 +0100

    Set up Node.js and React envs

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors'); //to allow cross-origin requests
+
+const app = express();
+app.use(express.json()); // allows us to parse incoming requests with JSON payloads
+app.use(cors({origin: true}));
+
+const db = require('mysql').createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Melissa_01",
+    database: "test"
+})
+
+app.get("/", (req, res) => {
+    res.json("Hello World!");
+});
+
+app.get("/books", (req, res) => {
+    const query = "SELECT * FROM books";
+    db.query(query, (err, result) => {
+        if(err) {
+            res.json({error: err});
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post("/books", (req, res) => {
+    const query = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?, ?, ?)";
+    const values = [req.body.title, req.body.desc, req.body.cover];
+
+    db.query(query, values, (err, result) => {
+        if(err) {
+            res.json({error: err});
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.listen(8000);
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
